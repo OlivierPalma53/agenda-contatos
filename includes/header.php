d4fcb856c155b9f95f63f9faeac4372984324eab
Olivier Palma <olivierpalmasouza@gmail.com>
2017-08-16 16:23:35 -0300

    Criando o Header e o Footer

---
+++ includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="lib/materialize/css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      <nav>
+      <div class="nav-wrapper">
+        <a href="index.php" class="brand-logo">Agenda de contatos</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a class="btn-floating btn-large halfway-fab waves-effect waves-light teal" href="inserir.php">
+              <i class="material-icons">add</i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      </nav>
